4b62b66f0f6d8df728824a12751d3022d7191667
nriab0 <bbarry792@gmail.com>
2025-04-06 23:41:45 +0100

    no cacheing
    global site wide

--- header.php
+++ header.php
@@ -1,4 +1,9 @@
 <?php
+    //No cahing - global implemetnation
+    header("Cache-Control: no-cache, no-store, must-revalidate");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+
     require_once 'functions.php';  
     require_once 'csrf.php';
     
